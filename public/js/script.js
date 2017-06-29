7d7890a24f505d1c1d9d8c2991e7c5dc20c0e736
Quintisimo <quintuscardozo13@gmail.com>
2017-06-29 22:29:29 +1000

    Added calendar with styling to events page

--- public/js/script.js
+++ public/js/script.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
   $('.game').click(function() {
     $(this).children('.game-details').slideToggle();
   });
+
+  $('#calendar').fullCalendar({
+    header: {
+      left: '',
+      center: 'title',
+      right: 'today prev,next'
+    },
+    googleCalendarApiKey: 'API_KEY',
+    events: {
+        googleCalendarId: 'CALENDAR_ID'
+    },
+    eventClick: function(event) {
+      window.open(event.url, 'gcalevent', 'width=700,height=600');
+      return false;
+    }
+  });
 });
