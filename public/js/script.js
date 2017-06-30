9e7da5dd13948037e65826b65a5a64e73f3d4e7e
Quintisimo <quintuscardozo13@gmail.com>
2017-06-30 18:31:11 +1000

    Added mobile header

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,21 @@ $(document).ready(function() {
     $(this).children('.game-details').slideToggle();
   });
 
+  $('#mobile-header-toggle').click(function() {
+    $this = $(this);
+    if ($('#mobile-header').is(':visible')) {
+      $this.fadeOut(500, function() {
+        $this.text('☰').fadeIn(500);
+      });
+    } else {
+      $this.fadeOut(500, function(){
+        $this.text('✕').fadeIn(500);
+      });
+    }
+    $('#mobile-header').slideToggle(function() {
+    });
+  });
+
   $('#calendar').fullCalendar({
     header: {
       left: '',
