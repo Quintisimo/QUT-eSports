d252edde3a3b77ec8c3ccb347dc483221b9a8f2c
Quintisimo <quintuscardozo13@gmail.com>
2017-07-03 14:58:57 +1000

    Added google map and changed contact button

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,36 @@
-$(document).ready(function() {
-  $('.game').click(function() {
-    $(this).children('.game-details').slideToggle();
+function initMap() {
+  var location = {
+    lat: -27.477356,
+    lng: 153.028423
+  };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: location
   });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
 
-  $('#mobile-header-toggle').click(function() {
-    $this = $(this);
-    if ($('#mobile-header').is(':visible')) {
-      $this.fadeOut(500, function() {
-        $this.text('☰').fadeIn(500);
-      });
-    } else {
-      $this.fadeOut(500, function(){
-        $this.text('✕').fadeIn(500);
-      });
-    }
-    $('#mobile-header').slideToggle('slow');
+(function($) {
+  $(document).ready(function() {
+    $('.game').click(function() {
+      $(this).children('.game-details').slideToggle();
+    });
+
+    $('#mobile-header-toggle').click(function() {
+      $this = $(this);
+      if ($('#mobile-header').is(':visible')) {
+        $this.fadeOut(500, function() {
+          $this.text('☰').fadeIn(500);
+        });
+      } else {
+        $this.fadeOut(500, function(){
+          $this.text('✕').fadeIn(500);
+        });
+      }
+      $('#mobile-header').slideToggle('slow');
+    });
   });
-});
+}(jQuery));
